46e3b146dfe2f07e2c3cf77173e793324ba3313a
Vikash Kumar <72787966+VikashKumarCv@users.noreply.github.com>
2024-12-08 09:53:31 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-
+#Baic
 DATABASE_URL = "postgresql+asyncpg://postgres:1234@localhost/book_management"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
